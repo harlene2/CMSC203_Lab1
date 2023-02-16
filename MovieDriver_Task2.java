d4b8b88930260fe113dce5e22b681b283668f77b
harlene2 <123613263+harlene2@users.noreply.github.com>
2023-02-16 15:02:07 -0500

    Update MovieDriver_Task2.java

--- MovieDriver_Task2.java
+++ MovieDriver_Task2.java
@@ -18,35 +18,43 @@ import java.util.*;
 public class MovieDriver_Task2 {
 	public static void main(String[] args) {
 		Scanner myObj = new Scanner(System.in);
+
+		// Loop to create and display movies
 		while (true) {
 			Movie movie = new Movie();
-			
+
+			// Read user input for movie title and set the title
 			System.out.print("Enter the name of a movie: ");
 			String title = myObj.nextLine();
 			movie.setTitle(title);
-			
+
+			// Read user input for movie rating and set the rating
 			System.out.print("Enter the rating of the movie: ");
 			String rating = myObj.nextLine();
 			movie.setRating(rating);
-			
+
+			// Read user input for number of tickets sold and set the number of tickets sold
 			System.out.print("Enter the number of tickets sold for this movie: ");
 			int SoldTickets = Integer.parseInt(myObj.nextLine());
 			movie.setSoldTickets(SoldTickets);
-			
+
+			// Display the movie information
 			System.out.println(movie);
-			
+
+			// Ask the user if they want to enter another movie
 			System.out.println("Do you want to enter another? (y or n)");
 			String option = myObj.nextLine();
-			
+
+			// Continue or break the loop based on user input
 			if (option.equalsIgnoreCase("y")) {
 				continue;
-			}
-			else {
+			} else {
 				break;
 			}
 		}
+
+		// Display goodbye message
 		System.out.println("Goodbye");
 		myObj.close();
-
 	}
 }
